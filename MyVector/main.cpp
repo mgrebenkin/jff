8c812e460f42eb8fb4b173ffc15746377dcad165
Mikhail Grebenkin <grebenkinmd@gmail.com>
2020-09-20 23:09:31 +0300

    Initial Commit

---
+++ MyVector/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MyVector
+//
+//  Created by Михаил Гребенкин on 20.09.2020.
+//  Copyright © 2020 Michael Grebenkin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

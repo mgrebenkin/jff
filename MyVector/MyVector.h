a0b1fff50a4d9115fb5de9178c84d9b009ec0b36
Mikhail Grebenkin <grebenkinmd@gmail.com>
2020-10-03 11:11:04 +0300

    MyVector can store objects. - Changed memory allocation - Added append() method - Added [] const operator - Added move push_back() method - Changed move and copy = operators - Changed copy push_back() method - Added Vector3 class for testing

--- MyVector/MyVector.h
+++ MyVector/MyVector.h
@@ -18,86 +18,113 @@ public:
     MyVector():
     m_data(nullptr),
     m_size(0),
-    m_blocks_amount(0){
-        //resize();
+    m_blocks_amount(0),
+    m_capacity(0){
     }
     MyVector(std::size_t count):
-    m_data(nullptr){
-        m_size = count;
-        resize();
+    m_data(nullptr), m_size(0), m_blocks_amount(0){
+        m_capacity = count;
+        allocate(m_capacity/_BLOCK_SIZE + m_capacity%_BLOCK_SIZE?1:0);
 
-    }//                                                    |1 1 1 1 1 | o o o o o| o o o o o |
+    }
+    void Clear(){
+        for (int i = 0; i< m_size; i++){
+            m_data[i].~T();
+        }
+        m_size = 0;
+    }
     ~MyVector(){
+        Clear();
         delete[] m_data;
     }
     T& operator[](std::size_t pos){
         return m_data[pos];
     }
-    std::size_t get_size(){return m_size;}
-    void pop_back(){
+    T& operator[](std::size_t pos) const{
+        return m_data[pos];
+    }
+    
+    std::size_t get_size() const {return m_size;}
+    
+    T& pop_back(){
         if(m_size > 0){
             m_size--;
-            resize();
+            return m_data[m_size];
+            
         }
     }
     void push_back(const T& elem){
+        if(m_size>=m_capacity) allocate(1);
+        m_data[m_size] = elem;
         m_size++;
-        resize();
-        m_data[m_size-1] = elem;
+        DBMSG("Copy pushed");
     }
-    void printout(){
-        if(m_size>0){
-            std::cout<<"|";
-            for(int i = 0; i<m_size; i++){
-                std::cout<<m_data[i]<<"|";
-            }
-        }else std::cout<<"Empty";
-        std::cout<<std::endl;
+    void push_back(T&& elem){
+        if(m_size>=m_capacity) allocate(1);
+        m_data[m_size] = std::move(elem);
+        m_size++;
+        DBMSG("Move pushed");
     }
-    MyVector& operator=(MyVector& other){
-        this->m_size = other.m_size;
-        resize();
-        memcpy(this->m_data, other.m_data, sizeof(T)*other.m_size);
+    MyVector& operator=(const MyVector& other){
+        Clear();
+        if(m_capacity<other.m_size) allocate(blocks_in_size(other.m_size - m_capacity));
+        for(size_t i = 0; i<other.m_size; i++)
+            m_data[i] = other.m_data[i];
+        m_size = other.m_size;
+        DBMSG("Copy = operator ");
         return *this;
     }
     MyVector& operator=(MyVector&& other){
-        if (this->m_data != nullptr) delete[] this->m_data;
-        this->m_data = other.m_data;
-        this->m_size = other.m_size;
-        this->m_blocks_amount = other.m_blocks_amount;
-        other.m_data = nullptr;
-        other.m_size() = 0;
+        Clear();
+        allocate(-m_capacity);
+        m_data = other.m_data;
+        m_size = other.m_size;
+        m_capacity = other.m_capacity;
+        m_blocks_amount = other.m_blocks_amount;
+        other.m_size = 0;
         other.m_blocks_amount = 0;
+        other.m_capacity = 0;
+        other.m_data = nullptr;
+        DBMSG("Move = operator");
         return *this;
     }
-    void append(MyVector& other){
+    MyVector<T>& append(const MyVector& other){
+        if(m_size+other.m_size>m_capacity)
+            allocate(blocks_in_size(m_size + other.m_size - m_capacity));
+        for(size_t i = 0; i<other.m_size; i++) m_data[m_size+i] = other.m_data[i];
         this->m_size+=other.m_size;
-        resize();
-        
+        return *this;
     }
 private:
     T* m_data;
     std::size_t m_size;
     std::size_t m_blocks_amount;
-    bool resize(){
-        if((m_blocks_amount*_BLOCK_SIZE>=m_size)&&
-           (m_blocks_amount*_BLOCK_SIZE<m_size+_BLOCK_SIZE)){
-            //DBMSG("Resize Check: did nothing");
-            return false;
-        }else{
-            //DBMSG("Did resize from: "); DBMSG(m_blocks_amount);
-            m_blocks_amount = (m_size/_BLOCK_SIZE) + (m_size%_BLOCK_SIZE?1:0);
-            T* old_mem = m_data;
-            m_data = new T[m_blocks_amount*_BLOCK_SIZE];
-            if(old_mem != nullptr){
-                memcpy(m_data, old_mem, sizeof(T)*m_size);
-                delete[] old_mem;
-            }
-            //DBMSG("To"); DBMSG(m_blocks_amount);
-            return true;
+    std::size_t m_capacity;
+    void allocate(long long blocks){
+        if(m_blocks_amount + blocks<=0){
+            for(size_t i = 0; i<m_size; i++) m_data[i].~T(); //????
+            delete[] m_data;
+            m_blocks_amount = 0;
+            m_size = 0;
+            m_capacity = 0;
+            return;
+        } else {
+            m_blocks_amount +=blocks;
+            m_capacity = m_blocks_amount*_BLOCK_SIZE;
+            T* new_data = new T[m_capacity];
+            m_size = m_size<=m_capacity?m_size:m_capacity;
+            for(size_t i = 0; i<m_size; i++)
+                new_data[i] = std::move(m_data[i]);
+            delete[] m_data;
+            m_data = new_data;
+            return;
         }
+        
+        
     }
-    
+    static size_t blocks_in_size(size_t size){
+        return size/_BLOCK_SIZE + size%_BLOCK_SIZE?1:0;
+    }
 };
 
 
--- MyVector/main.cpp
+++ MyVector/main.cpp
@@ -7,9 +7,78 @@
 //
 
 #include <iostream>
+#include "MyVector.h"
+struct Vector3{
+    float x;
+    float y;
+    float z;
+    std::string name;
+    Vector3():x(0), y(0), z(0), name("No name"){
+        std::cout<<"Vector3 Constructed no args\n";
+    }
+    Vector3(float ax, float ay, float az):
+    name("No name"){
+        x = ax;
+        y = ay;
+        z = az;
+        std::cout<<"Vector3 Constructed with args but no name\n";
+    }
+    Vector3(float ax, float ay, float az, std::string aname){
+        x = ax;
+        y = ay;
+        z = az;
+        name = aname;
+        std::cout<<"Vector3 Constructed with args\n";
+    }
+    
+    Vector3(const Vector3& other):
+    x(other.x), y(other.y), z(other.z), name(other.name){
+        std::cout<<"Vector3 Copied\n";
+    }
+    Vector3(Vector3&& other):
+    x(other.x), y(other.y), z(other.z){
+        name = std::move(other.name);
+        std::cout<<"Vector3 Moved\n";
+    }
+    Vector3& operator=(const Vector3& other ){
+        x = other.x;
+        y = other.y;
+        z = other.z;
+        name = other.name;
+        std::cout<<"Vector3 Copy = operator\n";
+        return *this;
+    }
+    Vector3& operator=(Vector3&& other){
+        x = other.x;
+        y = other.y;
+        z = other.z;
+        name = std::move(other.name);
+        std::cout<<"Vector3 Move = operator\n";
+        return *this;
+    }
+    void print(){
+        std::cout<<"Vector3 name:"<<name<<std::endl;
+        std::cout<<":"<<x<<":"<<y<<":"<<z<<":"<<std::endl;
+        std::cout<<"---------------"<<std::endl;
+    }
+};
+
+void print_vector(const MyVector<Vector3>& v){
+    for(std::size_t i = 0; i<v.get_size(); i++)
+        v[i].print();
+    std::cout<<"~~~~~~~~~~~~~~~~~~~"<<std::endl;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Vector3 x1(1,1,1, "Ones"), x2(2,2,2, "Twos"), x5(3,3,3, "Threes");
+    Vector3 x3(x1);
+    Vector3 x4(x2);
+    MyVector<Vector3> c(3);
+    MyVector<Vector3> d;
+    c.push_back(x1); c.push_back(x2);c.push_back(Vector3(5,5,5, "Fives"));
+    print_vector(c);
+    d = c;
+    print_vector(d);
+    print_vector(c.append(d));
     return 0;
 }
